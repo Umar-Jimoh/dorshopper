d91860064d83bf874777308239c19b3559675058
Umar Jimoh <umarjimoh@hotmail.com>
2025-01-02 11:53:45 +0100

    update product migration

--- database/migrations/2025_01_01_101822_create_products_table.php
+++ database/migrations/2025_01_01_101822_create_products_table.php
@@ -18,6 +18,9 @@ return new class extends Migration
             $table->string('slug', 2000);
             $table->longText('description');
             $table->foreignId('department_id')->index()->constrained('departments');
+            $table->foreignId('category_id')->index()->constrained('categories');
+            $table->decimal('price', 20, 4);
+            $table->string('status')->index();
             $table->integer('quantity')->nullable();
             $table->foreignIdFor(User::class, 'created_by');
             $table->foreignIdFor(User::class, 'updated_by');
