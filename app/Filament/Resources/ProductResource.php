e2c9712c5cb81a5e9f87773d1a46f6769436abdf
Umar Jimoh <umarjimoh@hotmail.com>
2025-01-03 10:20:15 +0100

    Create Product form to add new products

---
+++ app/Filament/Resources/ProductResource.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ProductResource\Pages;
+use App\Filament\Resources\ProductResource\RelationManagers;
+use App\Models\Product;
+use App\ProductStatusEnum;
+use Filament\Forms;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ProductResource extends Resource
+{
+    protected static ?string $model = Product::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('title')
+                    ->live(onBlur: true)
+                    ->required()
+                    ->afterStateUpdated(function (string $operation, $state, callable $set) {
+                    $set('slug', Str::slug($state));
+                }),
+                TextInput::make('slug')
+                    ->required(),
+                Select::make('department_id')
+                    ->relationship('department', 'name')
+                    ->label(__('Department'))
+                    ->preload()
+                    ->searchable()
+                    ->required()
+                    ->reactive() // Makes the field reactive to changes
+                    ->afterStateUpdated(function(callable $set) {
+                        $set('category_id', null); // Reset category when department changes
+                    }),
+                Select::make('category_id')
+                    ->relationship(
+                        name: 'category',
+                        titleAttribute: 'name',
+                        modifyQueryUsing: function(Builder $query, callable $get) {
+                            $departmentId = $get('department_id');
+                            if ($departmentId) {
+                                $query->where('department_id', $departmentId);
+                            }
+                        }
+                    )
+                    ->label('Category')
+                    ->preload()
+                    ->searchable()
+                    ->required(),
+                RichEditor::make('description')
+                        ->toolbarButtons([
+                        'blockquote',
+                        'bold',
+                        'bulletList',
+                        'h2',
+                        'h3',
+                        'italic',
+                        'link',
+                        'orderedList',
+                        'redo',
+                        'strike',
+                        'underline',
+                        'undo',
+                        'table',
+                        ])
+                    ->columnSpan(2)
+                    ->required(),
+                TextInput::make('price')
+                    ->numeric()
+                    ->required(),
+                TextInput::make('quantity')
+                    ->integer(),
+                Select::make('status')
+                    ->options(ProductStatusEnum::labels())
+                    ->default(ProductStatusEnum::Draft->value)
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListProducts::route('/'),
+            'create' => Pages\CreateProduct::route('/create'),
+            'edit' => Pages\EditProduct::route('/{record}/edit'),
+        ];
+    }
+}
