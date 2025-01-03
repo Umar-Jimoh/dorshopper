1c0569344dcc52addead7833d0273d161a163651
Umar Jimoh <umarjimoh@hotmail.com>
2025-01-03 18:38:52 +0100

    Change product and image upload icon

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -29,7 +29,7 @@ class ProductResource extends Resource
 {
     protected static ?string $model = Product::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-s-queue-list';
 
     protected static SubNavigationPosition $subNavigationPosition = SubNavigationPosition::End;
 
